0ba373c72180193018a05bd399d5aa90039c0a35
Alexsik76 <alex@jurist.vn.ua>
2021-05-26 20:57:13 +0300

    added error handlers, work with models.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 from config import app_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
+from flask_mail import Mail
 from flaskext.markdown import Markdown
 
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
+mail = Mail()
 
 
 def create_app(test_config=False):
@@ -17,13 +23,12 @@ def create_app(test_config=False):
         app.config.from_object(app_config['develop'])
 
     csrf.init_app(app)
+    db.init_app(app)
+    login.init_app(app)
+    mail.init_app(app)
+    Markdown(app)
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
-
-    Markdown(app)
-
-    db.init_app(app)
-
     # from app.create_db import init_app
     # init_app(app)
 
---
+++ app/auth/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
