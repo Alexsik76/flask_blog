d86cb2f366113a122b9cbd829fe464aa79fd8b30
Alexsik76 <alex@jurist.vn.ua>
2021-06-01 20:44:04 +0300

    mail is works 2

--- app/auth/email.py
+++ app/auth/email.py
@@ -5,23 +5,26 @@ from flask_mail import Message
 from app import mail
 
 
-def send_async_email(msg):
-    with current_app.app_context():
-        mail.send(msg)
-
-
-def send_registration_email(email):
-    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
-    token = serializer.dumps(email, salt=current_app.config['SECURITY_PASSWORD_SALT'])
-    msg = Message(subject='New user', recipients=[email])
-    msg.body = render_template('email/register.txt', token=token)
-    Thread(target=send_async_email, args=(msg,)).start()
-
-
-# async def send_registration_email(email):
+# def send_async_email(msg):
+#     mail.send(msg)
+#
+#
+# async def send_async_registration_email(email):
 #     serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
 #     token = serializer.dumps(email, salt=current_app.config['SECURITY_PASSWORD_SALT'])
 #     msg = Message(subject='New user', recipients=[email])
 #     msg.body = render_template('email/register.txt', token=token)
-#     with current_app.app_context():
-#         await mail.send(msg)
+#     send_async_email(msg)
+
+
+def send_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+async def send_registration_email(email):
+    serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    token = serializer.dumps(email, salt=current_app.config['SECURITY_PASSWORD_SALT'])
+    msg = Message(subject='New user', recipients=[email])
+    msg.body = render_template('email/register.txt', token=token)
+    Thread(target=send_email, args=(current_app._get_current_object(), msg)).start()
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -5,17 +5,16 @@ from app.auth.email import send_registration_email
 
 
 @bp.route('/signup', methods=['GET', 'POST'])
-def signup():
+async def signup():
     form = SignUpForm()
     print(form.email.data, form.validate_on_submit())
     if form.validate_on_submit():
-        send_registration_email(form.email.data)
+        await send_registration_email(form.email.data)
         flash('To continue registration, follow the link in the letter.')
         return redirect(url_for('main.index'))
-
     return render_template('auth/signup.html', form=form)
 
 
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
+@bp.route('/register/<token>', methods=['GET', 'POST'])
+def register(token):
     pass
