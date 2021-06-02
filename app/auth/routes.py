ca6cdd82c291ce53b2ea1c7c6400e75caec547ef
Alexsik76 <alex@jurist.vn.ua>
2021-06-02 09:07:23 +0300

    database works

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,16 +3,21 @@ from app.auth import bp
 from app.auth.forms import SignUpForm, RegistrationForm
 from app.auth.email import send_registration_email
 from itsdangerous import URLSafeTimedSerializer
+from app.models import User
+from app import db
 
 
 @bp.route('/signup', methods=['GET', 'POST'])
 async def signup():
     form = SignUpForm()
-    print(form.email.data, form.validate_on_submit())
-    if form.validate_on_submit():
+    is_busy = bool(User.query.filter_by(email=form.email.data).first())
+    if form.validate_on_submit() and not is_busy:
         await send_registration_email(form.email.data)
-        flash('To continue registration, follow the link in the letter.')
+        flash('To continue registration, follow the link in the letter.', 'info')
         return redirect(url_for('main.index'))
+    elif is_busy:
+        flash(f'The email: {form.email.data} is used.', 'danger')
+
     return render_template('auth/signup.html', form=form)
 
 
@@ -23,6 +28,14 @@ def register(token):
     email = serializer.loads(token, salt=current_app.config['SECURITY_PASSWORD_SALT'])
     form.email.data = email
     if form.validate_on_submit():
-        flash('You can log in')
+        new_user = User(
+            email=email,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data
+        )
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You can log in', 'success')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
