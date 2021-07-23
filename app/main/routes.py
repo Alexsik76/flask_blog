fae20786d87e8cbfbe82e3b1fd585d8426e055ec
Alexsik76 <alex@jurist.vn.ua>
2021-07-23 12:21:05 +0300

    experiments with turbo-frame

--- app/main/routes.py
+++ app/main/routes.py
@@ -34,7 +34,9 @@ def create_post():
         img = form.img.data
         filename = secure_filename(img.filename)
         if os.path.splitext(filename)[1] != validate_image(img.stream):
-            return render_template('create_post.html', form=form), 422
+            print('file is not valid')
+            flash('Files content is not valid!', 'danger')
+            return render_template('create_post.html', form=form)
         img.save(os.path.join(current_app.config['UPLOAD_PATH'], current_user.get_id(), filename))
         new_post = Post(
             title=form.title.data,
