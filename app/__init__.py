cca86c57b039c54b43b09632e5fbc19e90ddb294
Alexsik76 <alex@jurist.vn.ua>
2021-07-26 21:59:41 +0300

    solved turbo

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import app_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
-from flask_login import LoginManager, user_logged_in
+from flask_login import LoginManager
 from flask_admin import Admin
 from flask_mail import Mail
 from turbo_flask import Turbo
