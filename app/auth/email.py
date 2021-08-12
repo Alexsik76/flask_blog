8204eabc4dd27bcaa16444c39878079187752299
Alexsik76 <alex@jurist.vn.ua>
2021-08-12 21:45:34 +0300

    added email sync

--- app/auth/email.py
+++ app/auth/email.py
@@ -13,11 +13,14 @@ def async_send_email(obj, msg):
             return e
 
 
-async def send_email(email, goal):
+def send_email(email, goal, sync=False):
     serializer = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     token = serializer.dumps(email, salt=current_app.config['SECURITY_PASSWORD_SALT'])
     subject, template = {'registration': ('New user', 'email/register.txt'),
                          'reset': ('Reset password', 'email/reset_password.txt')}[goal]
     msg = Message(subject=subject, recipients=[email])
     msg.body = render_template(template, token=token)
-    Thread(target=async_send_email, args=(current_app._get_current_object(), msg)).start()
+    if sync:
+        mail.send(msg)
+    else:
+        Thread(target=async_send_email, args=(current_app._get_current_object(), msg)).start()
