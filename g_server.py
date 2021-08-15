e06284dcf4b514b75778f24de02d80a84c8b01aa
Alexsik76 <alex@jurist.vn.ua>
2021-08-15 17:33:25 +0000

    working variant

--- g_server.py
+++ g_server.py
@@ -1,8 +1,7 @@
 from gevent import monkey
 monkey.patch_all()
 
-from gevent.pywsgi import WSGIServer
 from app import create_app
 
 app = create_app()
-WSGIServer(('127.0.0.1', 5000), app).serve_forever()
+
