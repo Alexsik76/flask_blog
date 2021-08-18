672a6e4416ba5333ee4856c7fc9ab1b077dd1919
Alexsik76 <alex@jurist.vn.ua>
2021-08-18 13:00:46 +0300

    added autohide alerts

--- app/static/main.js
+++ app/static/main.js
@@ -1,3 +1,10 @@
 $(window).on('beforeunload', function () {
     $.get($SCRIPT_ROOT + '_user_info');
 });
+setTimeout(function () {
+    let alertList = document.querySelectorAll('.alert');
+    alertList.forEach(function (alert) {
+        new bootstrap.Alert(alert).close();
+    })
+}, 3000);
+
